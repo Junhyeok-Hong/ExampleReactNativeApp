e2450b19d1943bf1d47ace16d5b30bd92293ae6a
Michael Hong <michaelhong@Michaels-Mini.cogeco.local>
2022-01-17 14:50:40 -0500

    Initial go at the listview

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import AddNewPetScreen from "./AddNewPetScreen.js";
 import examplePetProfileScreen from "./examplePetProfileScreen.js";
 
 export default function App() {
-  return examplePetProfileScreen();
+  //return examplePetProfileScreen();
   //return AddNewPetScreen();
   //return LoginScreen();
-  //return ListScreen();
+  return ListScreen();
 }
--- ListScreen.js
+++ ListScreen.js
@@ -6,9 +6,17 @@ The ListView is from Recycler List View --- THIS IS NOT YET WORKING ---
 */
 
 import React from "react";
-import { StatusBar, SafeAreaView } from "react-native";
 import GorgeousHeader from "react-native-gorgeous-header";
 import RecycleTestComponent from "./RecyclerListView.js";
+import {
+  StatusBar,
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+} from "react-native";
 
 const menuImage = require("./assets/menu.png");
 const searchImage = require("./assets/search.png");
@@ -26,7 +34,41 @@ export default function ListScreen() {
           title="My Pets ❤️"
           subtitle="List of your pets"
         />
+        <FlatList
+          data={[
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+            { image: menuImage },
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+        />
       </SafeAreaView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    height: Dimensions.get("window").height * 0.2,
+    width: Dimensions.get("window").height * 0.4,
+    borderRadius: 20,
+    borderWidth: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 32,
+  },
+});
