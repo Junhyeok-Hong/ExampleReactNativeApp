9d1d24e6587be46dcf00bc9b9036af248e5155b4
Michael Hong <michaelhong@Michaels-Mini.cogeco.local>
2022-02-03 11:24:00 -0500

    React Native Navigation Added

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/*
 import LoginScreen from "./LoginScreen.js";
 import ListScreen from "./ListScreen.js";
 import AddNewPetScreen from "./AddNewPetScreen.js";
@@ -8,4 +9,54 @@ export default function App() {
   //return AddNewPetScreen();
   //return LoginScreen();
   return ListScreen();
+}
+*/
+
+/* Learn about React Native Navigation: https://reactnavigation.org/ */
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push("Details")}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
